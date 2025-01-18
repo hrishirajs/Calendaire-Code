a4cb204f7329da661e7efecbc102dce822a8f2f4
hrishirajs <hrs150203@gmail.com>
2025-01-18 23:03:06 +0530

    Setting Authjs

--- calendaire/app/lib/auth.ts
+++ calendaire/app/lib/auth.ts
@@ -1,1 +1,17 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google"
+import Apple from "next-auth/providers/apple"
+import Slack from "next-auth/providers/slack"
+import Zoom from "next-auth/providers/zoom"
+import Nodemailer from "next-auth/providers/nodemailer"
+
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [GitHub,Google,Apple,Slack,Zoom,
+    Nodemailer({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+});
